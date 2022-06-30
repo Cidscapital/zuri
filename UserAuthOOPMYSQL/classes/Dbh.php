23a367ccc2c3e22313fd82f1015ad14c3134bd0d
Cidscaptal <jesse.jason2002@gmail.com>
2022-06-30 19:08:05 +0300

    Created dbh class

--- UserAuthOOPMYSQL/classes/Dbh.php
+++ UserAuthOOPMYSQL/classes/Dbh.php
@@ -1,5 +1,27 @@
 <?php
+//     create a class called `Dbh` with the following properties hostname,  username, password, dbname
 
+class Dbh{
+    private $host = "localhost";
+    private $user = "root";
+    private $pwd = "";
+    private $dbname = "zuriphp";
 
+    protected $connect;
+ 
+    public function __construct(){
+ 
+        if (!isset($this->connect)) {
+ 
+            $this->connect = new mysqli($this->host, $this->username, $this->password, $this->database);
+ 
+            if (!$this->connect) {
+                echo 'Cannot connect to database server';
+                exit;
+            }            
+        }    
+ 
+    }
+}
 
 ?>
